c0476dcc4538e56d600917a6abe67f84aa73c756
vojta <vojta@ne.cz>
2021-05-17 07:58:29 +0200

    Initial Commit

---
+++ MonumentalAppAzureCup/MonumentalAppAzureCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonumentalAppAzureCupApp.swift
+//  MonumentalAppAzureCup
+//
+//  Created by vojta on 17.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MonumentalAppAzureCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
